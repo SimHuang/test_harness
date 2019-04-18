222e6c1cdab40f128955a827f2c1fb0090fa2a87
Zuzanna Rybicka <rybicka.zuzanna@gmail.com>
2019-04-18 12:04:07 -0400

    Added try/catch for divide by zero in TestFunctor.cpp class
    really not sure if this is the correct way to do it though

--- test_harness/TestFunctor.cpp
+++ test_harness/TestFunctor.cpp
@@ -18,8 +18,23 @@ void hello(void) {
 	cout << "Hello world!" << endl;
 }
 
+int Division(int x, int y) {
+	if (y == 0) {
+		throw runtime_error("Dividing by zero error\n");
+	}
+	return x / y;
+}
+
 void divideByZero(void) {
-	cout << "Bad idea" << endl;
+	cout << "Bad idea to divide by zero" << endl;
+	try {
+		int result = Division(10, 0);
+		cout << "quotient: " << result << endl;
+	}
+	catch (runtime_error e) {
+		cout << "Exception: " << endl
+			<< e.what();
+	}
 }
 
 //creating and calling a queue of functions/functors/function pointers/lambdas
@@ -28,8 +43,8 @@ int TestFunctor::FunctorQueue(void) {
 	fn f1; //creating fn object f1
 	f1.id = 1;
 	f1.f = &hello; //assigning hello function to f1 object
-	cout << "First function: " << endl;
-	f1.f(); //runs the hello function
+	cout << "First function: hello" << endl;
+	//f1.f(); //runs the hello function
 	q.push(f1); //adds the function to the queue
 	cout << "Calling the function from front of queue" << endl;
 	fn f1called = q.front();
@@ -40,12 +55,11 @@ int TestFunctor::FunctorQueue(void) {
 	fn f2; 
 	f2.id = 2;
 	f2.f = &divideByZero; 
-	cout << "Second function: " << endl;
+	cout << "Second function: divbyzero" << endl;
 	f2.f(); 
 	q.push(f2); 
 	cout << "Calling the function from front of queue" << endl;
 	fn f2called = q.front();
-	f2called.f();
 	q.pop();
 
 	return 0;
