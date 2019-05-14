cfe649f7af83e86c2a60c51dc62f777fd127e979
Zuzanna Rybicka <rybicka.zuzanna@gmail.com>
2019-05-13 21:34:09 -0400

    made dll_three_new and updated xml to reflect new project names

---
+++ Dll_THREE_new/TestDriver.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include "framework.h"
+#include "TestDriver.h"
+#include "Test.h"
+#include <iostream>
+
+using namespace std;
+
+/*
+	This is responsible for executing the tests
+*/
+TESTDRIVER_API bool test()
+{
+	return OutOfRange();
+}
+
+/*
+	ITest is the interface between the testdriver and the test function.
+	It will invoke the test() function and the test function will invoke the
+	actual test and return a true or false.
+*/
+TESTDRIVER_API bool ITest()
+{
+	return test();
+}
