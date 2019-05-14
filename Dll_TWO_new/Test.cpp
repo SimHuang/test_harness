70ea4633e503937272e4207154ba2474b3e218a7
Zuzanna Rybicka <rybicka.zuzanna@gmail.com>
2019-05-13 21:26:28 -0400

    made dll_two a new dll_two_new

---
+++ Dll_TWO_new/Test.cpp
@@ -0,0 +1,23 @@
+#include "pch.h" // use pch.h in Visual Studio 2019
+#include <utility>
+#include <limits.h>
+#include "Test.h"
+#include <iostream>
+
+using namespace std;
+
+bool OverflowError() {
+	try {
+		const int x = 1000; //make this a very large number to get overflow exception
+		if (x > 100)
+			throw std::overflow_error("Overflow error has occured");
+		else {
+			int array[x];
+			cout << "Array is acceptable size" << endl;
+		}
+	}
+	catch (runtime_error e) {
+		return false;
+	}
+	return true;
+}
